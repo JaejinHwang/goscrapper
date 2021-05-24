cc4936c1d703c5828bc2c323ab79dfb55fc7f475
JaejinHwang <hjj4756@kaist.ac.kr>
2021-05-24 23:51:01 +0900

    #1.2 Deposit and Withdraw
    build deposit, withdraw and check balance method

--- accounts/accounts.go
+++ accounts/accounts.go
@@ -1,13 +1,36 @@
 package accounts
 
+import "errors"
+
 //define struct Account
 type Account struct {
 	owner string
 	balance int
 }
 
+var errNomoney = errors.New("amount of withdraw is bigger than your balance")
+
 //function CreateAccount makes new struct Account
 func CreateAccount (owner string) *Account {
 	account := Account{owner: owner, balance: 0}
 	return &account
+}
+
+//method Deposit makes input amount of deposit into account
+func (a *Account) Deposit (amount int) {
+	a.balance += amount
+}
+
+//method CheckBalance checks balance of account
+func (a Account) CheckBalance () int {
+	return a.balance
+}
+
+//method Withdraw makes input amount of withdraw into account
+func (a *Account) Withdraw (amount int) error {
+	if a.balance < amount {
+		return errNomoney
+	}
+	a.balance -= amount
+	return nil
 }
--- main.go
+++ main.go
@@ -8,5 +8,12 @@ import (
 
 func main() {
 	account := accounts.CreateAccount("Jay")
-	fmt.Println(account)
+	account.Deposit(1000)
+	account.Deposit(3000)
+	account.Withdraw(2000)
+	err := account.Withdraw(5000)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(account.CheckBalance())
 }
