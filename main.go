8e7621f1f32dcae8cd42ed627f3b35eef541139e
JaejinHwang <hjj4756@kaist.ac.kr>
2021-05-25 18:11:06 +0900

    #2.1 URL Checker
    create hitURL functions that search input url and return error code if it exists

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"net/http"
+)
 
-	"github.com/JaejinHwang/goscrapper/dictionarys"
+var (
+	errRequestFailed = errors.New("request failed")
 )
 
 func main() {
@@ -23,19 +27,53 @@ func main() {
 
 	// Code about commit #1.4 ~ #1.5
 
-	dictionary := dictionarys.Dictionary{}
-	exword := "hello"
-	dictionary.Add(exword, "greeting")
-	result := dictionary.Update("xdd", "first")
-	if result != nil {
-		fmt.Println(result)
+	// dictionary := dictionarys.Dictionary{}
+	// exword := "hello"
+	// dictionary.Add(exword, "greeting")
+	// result := dictionary.Update("xdd", "first")
+	// if result != nil {
+	// 	fmt.Println(result)
+	// }
+	// word, _ := dictionary.Search(exword)
+	// fmt.Println(word)
+	// fmt.Println(dictionary)
+	// result2 := dictionary.Delete("second")
+	// if result2 != nil {
+	// 	fmt.Println(result2)
+	// }
+	// fmt.Println(dictionary)
+	// }
+
+	var results = make(map[string]string)
+
+	urls := [] string {
+		"https://www.google.com/",
+		"https://www.youtube.com/",
+		"https://www.airbnb.com/",
+		"https://www.amazon.com/",
+		"https://www.instagram.com/",
+		"https://www.soundcloud.com/",
 	}
-	word, _ := dictionary.Search(exword)
-	fmt.Println(word)
-	fmt.Println(dictionary)
-	result2 := dictionary.Delete("second")
-	if result2 != nil {
-		fmt.Println(result2)
+
+	for _, url := range urls {
+		result := "OK"
+		err := hitURL(url)
+		if err != nil{
+			result = "FAILED"
+		}
+		results[url] = result
 	}
-	fmt.Println(dictionary)
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
+}
+
+func hitURL (url string) error {
+	fmt.Println("Checking: ", url)
+	resp, err := http.Get(url)
+	if err != nil || resp.StatusCode >= 400 {
+		fmt.Println(err, resp.StatusCode)
+		return errRequestFailed
+	}
+	return nil
+}
